a7bd4712af2115030f17790cb79baccfb0dd110b
Vidhi Kumar <vidhikumar2000@gmail.com>
2024-01-14 00:00:24 +0530

    initial commit

---
+++ mlprod/config_schemas/config_schema.py
@@ -0,0 +1,10 @@
+from hydra.core.config_store import ConfigStore
+from pydantic.dataclasses import dataclass
+
+@dataclass
+class Config:
+    pass
+
+def setup_config() -> None:
+    cs = ConfigStore.instance() 
+    cs.store(name="config_schema", node=Config)
---
+++ mlprod/entrypoint.py
@@ -0,0 +1,9 @@
+from mlprod.config_schemas.config_schema import Config
+from mlprod.utils.config_utils import get_config
+
+@get_config(config_path="../configs", config_name="config")
+def entrypoint(config: Config) -> None:
+    print(config)
+    
+if __name__ == "__main__":
+    entrypoint()
