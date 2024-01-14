eeaa8fda63f04cae138ff94f162f9db8ad4b21d1
Vidhi Kumar <vidhikumar2000@gmail.com>
2024-01-14 13:08:07 +0530

    added functionality to initialize DVC

---
+++ mlprod/utils/utils.py
@@ -0,0 +1,11 @@
+import socket
+import logging
+import subprocess
+
+
+def get_logger(name: str) -> logging.Logger:
+    return logging.getLogger(f"[{socket.gethostname()}] {name}")
+
+
+def run_shell_command(cmd: str) -> str:
+    return subprocess.run(cmd, text=True, capture_output=True, check=True, shell=True).stdout
--- mlprod/version_data.py
+++ mlprod/version_data.py
@@ -1,9 +1,10 @@
 from mlprod.config_schemas.config_schema import Config
 from mlprod.utils.config_utils import get_config
+from mlprod.utils.data_utils import initialize_dvc
 
 @get_config(config_path="../configs", config_name="config")
-def entrypoint(config: Config) -> None:
-    print(config)
+def version_data(config: Config) -> None:
+    initialize_dvc()
     
 if __name__ == "__main__":
-    entrypoint()
+    version_data()
