0ceda3c3dc33a57f5086539e3387204e44a47915
Vidhi Kumar <vidhikumar2000@gmail.com>
2024-01-28 23:13:46 +0530

    Updated version of data from v0 to v1

--- mlprod/utils/utils.py
+++ mlprod/utils/utils.py
@@ -1,11 +1,15 @@
 import socket
 import logging
 import subprocess
+# from mlprod.utils.utils import get_logger
+from pathlib import Path
 
+# DATA_UTILS_LOGGER = get_logger(Path(__file__).name)
 
 def get_logger(name: str) -> logging.Logger:
     return logging.getLogger(f"[{socket.gethostname()}] {name}")
 
 
 def run_shell_command(cmd: str) -> str:
+    print(f"Running shell command: {cmd}")
     return subprocess.run(cmd, text=True, capture_output=True, check=True, shell=True).stdout
